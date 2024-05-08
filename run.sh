d6a702bc1fe76dfc4a8cf30e7afb490e64516593
Ash <101825086+ashsProjects@users.noreply.github.com>
2024-05-08 07:35:29 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,4 @@
 #!/bin/sh
-cd ~/Documents/cs455/Assign2
-
-if module list 2>&1 | grep -q "courses/cs455"; then :
-else
-    module unload courses
-    module load courses/cs455
-fi
-
 gradle clean
 gradle build
 
